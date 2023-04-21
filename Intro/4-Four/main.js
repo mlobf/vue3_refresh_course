a480980ed199bb35d426e216863653c1f00e052d
mlobf <marcoslemeborbafilho@gmail.com>
2023-04-21 16:39:18 -0300

    Add boiler Plate for Computer Properties

--- Intro/4-Four/main.js
+++ Intro/4-Four/main.js
@@ -1,10 +1,41 @@
 const app = Vue.createApp({
   data() {
     return {
-      name: "Marcos",
+      myObj: {
+        users: [
+          {
+            id: 0,
+            name: "Marcos",
+            profession: "Developer",
+          },
+          {
+            id: 1,
+            name: "Jane",
+            profession: "UI/UX",
+          },
+          {
+            id: 2,
+            name: "Bruce",
+            profession: "UI/UX",
+          },
+          {
+            id: 3,
+            name: "Albert",
+            profession: "Doctor",
+          },
+          {
+            id: 4,
+            name: "Alvaro",
+            profession: "Lawer",
+          },
+        ],
+      },
     };
   },
-  methods: {},
+  methods: {
+    methodOne() {},
+    methodTwo() {},
+  },
 });
 
 const mountedApp = app.mount("#app");
--- Intro/5-Five/main.js
+++ Intro/5-Five/main.js
@@ -1,40 +1,11 @@
 const app = Vue.createApp({
   data() {
     return {
-      myObj: {
-        users: [
-          {
-            id: 0,
-            name: "Marcos",
-            profession: "Developer",
-          },
-          {
-            id: 1,
-            name: "Jane",
-            profession: "UI/UX",
-          },
-          {
-            id: 2,
-            name: "Bruce",
-            profession: "UI/UX",
-          },
-          {
-            id: 3,
-            name: "Albert",
-            profession: "Doctor",
-          },
-          {
-            id: 4,
-            name: "Alvaro",
-            profession: "Lawer",
-          },
-        ],
-      },
+      myObj: {},
     };
   },
   methods: {
     methodOne() {},
-    methodTwo() {},
   },
 });
 
